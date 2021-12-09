a7568aeb2c2eb724860b23e1110e8a914a162cb1
huaweiwei <harvey0379@163.com>
2021-12-09 15:25:02 +0800

    ref reactive watch

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,22 @@
 <script setup>
 // This starter template is using Vue 3 <script setup> SFCs
 // Check out https://v3.vuejs.org/api/sfc-script-setup.html#sfc-script-setup
-import HelloWorld from './components/HelloWorld.vue'
+import HelloWorld from './components/HelloWorld.vue';
+
+function say() {
+  console.log('say some words');
+}
 </script>
 
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Hello Vue 3 + Vite" />
+  <HelloWorld msg="Hello Vue 3 + Vite" @say="say">
+    <template #name>
+      <span>Component: HelloWorld.vue</span>
+    </template>
+    <template #version>
+      <p>Version: 1.0.1</p>
+    </template>
+  </HelloWorld>
 </template>
 
 <style>
--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,40 +1,67 @@
 <script setup>
-import { ref } from 'vue'
+import { ref, reactive, defineProps, watch, defineEmits } from 'vue';
 
 defineProps({
-  msg: String
-})
+  msg: {
+    type: String,
+    required: true,
+  },
+});
 
-const count = ref(0)
+const emit = defineEmits(['say']);
+
+const data = ref({
+  video: {
+    title: 'learn vue3',
+    likes: 231,
+  },
+});
+
+const data1 = reactive({
+  video: {
+    title: 'learn vue3',
+    likes: 231,
+  },
+});
+
+const changeName = () => {
+  // data.value.video.title += '*';
+  // data.value.video.likes += 1;
+
+  // data1.video.title += '$';
+  // data1.video.likes -= 1;
+  console.log(emit);
+  emit('say');
+};
+
+// WARNING: `deep` must be true, or NOT WORK
+watch(
+  data,
+  (val, oldVal) => {
+    console.log('data', val, oldVal);
+  },
+  { deep: true },
+);
+
+watch(data1, (val, oldVal) => {
+  console.log('data1', val, oldVal);
+});
+
+watch(data1.video, (val, oldVal) => {
+  console.log('data1.video', val, oldVal);
+});
 </script>
 
 <template>
-  <h1>{{ msg }}</h1>
-
-  <p>
-    Recommended IDE setup:
-    <a href="https://code.visualstudio.com/" target="_blank">VSCode</a>
-    +
-    <a href="https://github.com/johnsoncodehk/volar" target="_blank">Volar</a>
-  </p>
-
-  <p>
-    <a href="https://vitejs.dev/guide/features.html" target="_blank">
-      Vite Documentation
-    </a>
-    |
-    <a href="https://v3.vuejs.org/" target="_blank">Vue 3 Documentation</a>
-  </p>
-
-  <button type="button" @click="count++">count is: {{ count }}</button>
-  <p>
-    Edit
-    <code>components/HelloWorld.vue</code> to test hot module replacement.
-  </p>
+  <slot name="name"></slot>
+  <slot name="version"></slot>
+  <p>msg: {{ msg }}</p>
+  <div>title: {{ data.video.title }}</div>
+  <div>likes: {{ data.video.likes }}</div>
+  <hr />
+  <div>title: {{ data1.video.title }}</div>
+  <div>likes: {{ data1.video.likes }}</div>
+  <button @click="changeName">update</button>
 </template>
 
-<style scoped>
-a {
-  color: #42b983;
-}
-</style>
+<style scoped></style>
