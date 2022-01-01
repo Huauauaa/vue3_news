bfffdb1248070198c2f7fb041ea454524bc941a9
huaweiwei <harvey0379@163.com>
2022-01-01 20:34:55 +0800

    emits

--- src/components/Child.vue
+++ src/components/Child.vue
@@ -1,14 +1,18 @@
 <script setup>
-import { ref } from 'vue';
+import { ref, getCurrentInstance } from 'vue';
+const instance = getCurrentInstance();
 const count = ref(0);
 
 defineExpose({ count });
+const emits = defineEmits(['say']);
 </script>
 <template>
   <fieldset>
     <legend>Child</legend>
+    {{ $attrs }}
     {{ count }}
     <button @click="count++">+</button>
+    <button @click="() => emits('say', 'hello')">echo</button>
   </fieldset>
 </template>
 <style scoped></style>
--- src/components/Parent.vue
+++ src/components/Parent.vue
@@ -6,12 +6,16 @@ const childRef = ref();
 const onDecrease = () => {
   childRef.value.count--;
 };
+
+function onSay(val) {
+  console.log('onSay', val);
+}
 </script>
 <template>
   <fieldset>
     <legend>Parent</legend>
     <button @click="onDecrease">-</button>
-    <Child ref="childRef" />
+    <Child ref="childRef" @say="onSay" name="c" />
   </fieldset>
 </template>
 <style scoped></style>
