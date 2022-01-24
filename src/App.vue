a5c248c45b6a910cad63d639c6508e37ced2234a
huaweiwei <harvey0379@163.com>
2022-01-24 08:39:12 +0800

    props default value

--- src/App.vue
+++ src/App.vue
@@ -18,6 +18,7 @@ import PiniaDemo from './components/demo/PiniaDemo.vue';
 import DayjsDemo from './components/demo/DayjsDemo.vue';
 import AntVX6 from './components/demo/AntVX6.vue';
 import ProgressBar from './components/ProgressBar.vue';
+import PropsTest from './components/syntax/PropsTest.vue';
 
 const isShowDemo = ref(true);
 
@@ -44,6 +45,10 @@ onMounted(async () => {
 
 <template>
   <div :style="variables">
+    <PropsTest name="null" :value="null" :value1="null" />
+    <PropsTest name="undefined" :value="undefined" :value1="undefined" />
+    <PropsTest name="normal" value="foo" value1="foo" />
+    <PropsTest name="empty" />
     <ProgressBar
       :data="[
         { label: 'a', value: 0 },
---
+++ src/components/syntax/PropsTest.vue
@@ -0,0 +1,16 @@
+<script setup>
+const props = defineProps({
+  name: { type: String, required: true },
+  value: { type: String, required: true },
+  value1: { type: String, required: false, default: 'default' },
+});
+</script>
+
+<template>
+  <fieldset>
+    <legend>{{ props.name }}</legend>
+    value: {{ props.value }}------value1: {{ props.value1 }}
+  </fieldset>
+</template>
+
+<style scoped></style>
