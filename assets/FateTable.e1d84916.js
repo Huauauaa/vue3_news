732a80af0928120db5425d8d82b5caed5f38fa41
Huauauaa <Huauauaa@users.noreply.github.com>
2022-04-14 05:43:04 +0000

    deploy: 83326ccfefa071615482e1758d832f847475ff10

--- assets/FateTable.e1d84916.js
+++
@@ -1,2 +0,0 @@
-import{_ as t}from"./index.d57207a7.js";import{o,c as a,a as e}from"./vendor.f5b30b0c.js";const r={},c=e("tr",null,[e("td",null,"1"),e("td",null,"2")],-1),n=[c];function l(s,_){return o(),a("table",null,n)}var u=t(r,[["render",l]]);export{u as default};
-//# sourceMappingURL=FateTable.e1d84916.js.map
