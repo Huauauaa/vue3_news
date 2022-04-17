387adcf847c5803abf37319ad55176d94b7a1154
Huauauaa <Huauauaa@users.noreply.github.com>
2022-04-17 14:43:40 +0000

    deploy: 2c1f648c7359c0cef9f18bf061030368620540ac

---
+++ assets/CountDemo.5b0e2623.js
@@ -0,0 +1,2 @@
+import{r as n,o as s,c as a,a as o,t as l}from"./index.b5a4bf8f.js";const r=o("legend",null,"CountDemo",-1),_={setup(u){const e=n(0);return(c,t)=>(s(),a("fieldset",null,[r,o("button",{onClick:t[0]||(t[0]=i=>e.value++)},l(e.value),1)]))}};export{_ as default};
+//# sourceMappingURL=CountDemo.5b0e2623.js.map
