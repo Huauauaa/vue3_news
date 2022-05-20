00304ad7cc1765fde4d478d37e28a03f3d6d641c
huaweiwei <harvey0379@163.com>
2022-05-20 19:19:46 +0800

    vue2

---
+++ vue2/src/views/slot-test/SlotTest.vue
@@ -0,0 +1,16 @@
+<script>
+import Wrapper from './Wrapper.vue';
+
+export default {
+  name: 'SlotTest',
+  components: {
+    Wrapper,
+  },
+};
+</script>
+
+<template>
+  <div>
+    <Wrapper> 1 </Wrapper>
+  </div>
+</template>
---
+++ vue2/src/views/slot-test/Wrapper.vue
@@ -0,0 +1,7 @@
+<template>
+  <div>
+    <slot></slot>
+    <slot></slot>
+    <slot name="footer"></slot>
+  </div>
+</template>
